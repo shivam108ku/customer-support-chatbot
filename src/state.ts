2dfecb64cdced8f9e1a8f7f9a1f7bca74004860a
Shivam <shivam108ku@gmail.com>
2026-04-29 12:31:42 +0530

    state define for all models

---
+++ src/state.ts
@@ -0,0 +1,10 @@
+import {
+  Annotation,
+  MessagesAnnotation,
+} from "@langchain/langgraph";
+
+export const StateAnnotation = Annotation.Root({
+  ...MessagesAnnotation.spec,
+  nextRepresentative: Annotation<string>,
+  refundAuthorized: Annotation<boolean>,
+});
