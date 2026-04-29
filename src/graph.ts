430f09c322ee0e302d716a0f86b4b49762614fb5
Shivam <shivam108ku@gmail.com>
2026-04-29 16:35:30 +0530

    creting function

---
+++ src/graph.ts
@@ -0,0 +1,17 @@
+import type { StateAnnotation } from "./state";
+
+function frontDeshSupport(state: typeof StateAnnotation.State){
+    
+    return state;
+}
+
+
+function marketingSupport(state: typeof StateAnnotation.State){
+    
+    return state;
+}
+
+function  learningSupport(state: typeof StateAnnotation.State){
+    
+    return state;
+}
